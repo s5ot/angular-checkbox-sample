7e4e5c8cbf6e94c5301d38d949369f412298ea5d
sogo.ohta <sogo.ohta@gmail.com>
2014-11-17 15:56:40 +0900

    init

--- app/app.js
+++ app/app.js
@@ -2,11 +2,42 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
+  //'ngRoute',
+  'ui.router',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
+config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/view1");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('view1', {
+      url: "/view1",
+      templateUrl: "view1/view1.html",
+      controller:  "View1Ctrl"
+    })
+    .state('state1.list', {
+      url: "/list",
+      templateUrl: "partials/state1.list.html",
+      controller:  "View1Ctrl"
+    })
+    .state('view2', {
+      url: "/view2",
+      templateUrl: "view2/view2.html",
+      controller:  "View2Ctrl"
+    })
+    .state('state2.list', {
+      url: "/list",
+      templateUrl: "partials/state2.list.html",
+    });
+});
+/*
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+*/
+
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,13 +1,15 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute', 'ui.bootstrap'])
+angular.module('myApp.view1', ['ui.bootstrap'])
 
+/*
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
+*/
 
 .controller('View1Ctrl', ['$scope', function($scope) {
   $scope.checkAll = function() {
